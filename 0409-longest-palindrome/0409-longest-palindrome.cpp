1162969bd76d7e321bbddb0dc8be38d1514ab8d6
Siddhi Bajpai <115869460+siddhi-bajpai@users.noreply.github.com>
2024-06-04 10:08:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (75.88%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    int longestPalindrome(string s){
-        vector<int> lower(26,0);
-        vector<int> upper(26,0);
+    int longestPalindrome(string s) {
+        vector<int>lower(26,0);
+        vector<int>upper(26,0);
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='a'){
+                lower[s[i]-'a']++;
+            }
+            else{
+                upper[s[i]-'A']++;
+            }
 
-        for(int i=0;i<s.length();i++){
-            if(s[i]>='a' && s[i]<='z')
-            lower[s[i]-'a']++;
-            else
-            upper[s[i]-'A']++;
         }
-        int count=0;
-        int odd=0;
+        int c=0;
+        int f=0;
         for(int i=0;i<26;i++){
             if(lower[i]%2==0){
-                count+=lower[i];
+                c += lower[i];
             }
             else{
-                count+=(lower[i]-1);
-                odd=1;
+                c += lower[i]-1;
+                f=1;
             }
             if(upper[i]%2==0){
-                count+=upper[i];
+                c += upper[i];
             }
             else{
-                count+=(upper[i]-1);
-                odd=1;
+                c += upper[i]-1;
+                f=1;
             }
+
         }
-        return count+odd;
+        return c+f;
+        
     }
 };
