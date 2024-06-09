7492483a3cbd1570ad457230d41a554225c9fa63
Siddhi Bajpai <115869460+siddhi-bajpai@users.noreply.github.com>
2024-06-09 10:30:31 +0530

    Time: 342 ms (40%), Space: 611.7 MB (40%) - LeetHub

--- 3422-find-the-n-th-value-after-k-seconds/3422-find-the-n-th-value-after-k-seconds.cpp
+++ 3422-find-the-n-th-value-after-k-seconds/3422-find-the-n-th-value-after-k-seconds.cpp
@@ -4,11 +4,16 @@ using namespace std;
 class Solution {
 public:
     int valueAfterKSeconds(int n, int k) {
-        vector<int> vec(n, 1);
-        for (int i = 1; i <= k; i++) {
+        vector<long long> vec(n, 1);
+        int MOD = 1000000007;
+        
+        for (int i = 0; i < k; i++) {
+            vector<long long> new_vec(n, 0);
+            new_vec[0] = vec[0];
             for (int j = 1; j < n; j++) {
-                vec[j] = (vec[j] + vec[j - 1]) % 1000000007;
+                new_vec[j] = (new_vec[j - 1] + vec[j]) % MOD;
             }
+            vec = new_vec;
         }
         return vec[n - 1];
     }
