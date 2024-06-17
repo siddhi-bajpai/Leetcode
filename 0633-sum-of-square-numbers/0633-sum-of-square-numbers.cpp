ac14f45d71228dc8bc37a4c4656b449550b7d88d
Siddhi Bajpai <115869460+siddhi-bajpai@users.noreply.github.com>
2024-06-17 13:51:24 +0530

    Time: 4 ms (46.7%), Space: 7 MB (91.95%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-       long long int i=0, j=sqrt(c);
-       if(j*j==c)
-       return true;
-       while(i<=j){
-        if(i*i + j*j == c)
-        return true;
-        else if(i*i + j*j < c)
-        i++;
-        else
-        j--;
-       }
-       return false;
+        long long int i=0;
+        long long int j=int(sqrt(c));
+        while(i<=j){
+            if(i*i + j*j == c)
+            return true;
+            else if(i*i + j*j < c)
+            i++;
+            else
+            j--;
+        }
+        return false;
     }
 };
